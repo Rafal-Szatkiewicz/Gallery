e14ac2176a7f2e3b6decc0fec83a6374a8192e5f
rafal-szatkiewicz <rafalszatkiewicz2@gmail.com>
2022-12-28 20:26:09 +0100

    reverted changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,20 @@ module.exports = {
     plugins: [
         new NodePolyfillPlugin()
     ],
-    
+    resolve: {
+      fallback: {
+        "fs": false,
+        "path": false,
+        "net": false,
+        "dns": false,
+        "tls": false,
+        "async_hooks": false,
+        "bson-ext": false,
+        "kerberos": false,
+        "@mongodb-js/zstd": false,
+        "snappy": false,
+        "aws4": false,
+        "mongodb-client-encryption": false,
+      }
+    }
 }
