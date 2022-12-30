2ad514c181ef380f20aa9f8473f3a1456926ad20
rafal-szatkiewicz <rafalszatkiewicz2@gmail.com>
2022-12-30 02:11:34 +0100

    fixed uploading

--- public/api/product.js
+++ public/api/product.js
@@ -10,8 +10,8 @@
 
   // external js: packery.pkgd.js, draggabilly.pkgd.js
   const mongodb = require('mongodb');
-  //const cloudinary = require('cloudinary');
-  const cloudinary = require('cloudinary-core').Cloudinary.new();
+  const cloudinary = require('cloudinary').v2;
+  //const cloudinary = require('cloudinary-core').Cloudinary.new();
   const express = require('express');
   const fileUpload = require('express-fileupload');
   const path = require('path');
@@ -26,7 +26,6 @@
     api_key: process.env.API_KEY, 
     api_secret: process.env.API_SECRET
   });
-  
   // Replace with your MongoDB connection string
   const mongoUrl = 'mongodb+srv://r3r2xh93:AQOhP7z9hcoo7TeO@gallery.uxrixqp.mongodb.net/gallery';
   
@@ -51,9 +50,12 @@
   
     // Get the uploaded file
     const file = req.files.image;
+    console.log(file.tempFilePath);
   
     // Upload the file to Cloudinary
-    const result = await cloudinary.uploader.upload(file.tempFilePath);
+    const result = await cloudinary.uploader.upload(file.tempFilePath).catch((error) =>{
+      console.log(error);
+    });;
     //console.log(result);
     const imageUrl = result.url;
     const version = result.version;
@@ -93,10 +95,8 @@
   
     const verify = await db.collection('users').findOne({username: username});
     //console.log(verify);
-    let verifyHash = false;
-    verifyHash = await bcrypt.compare(password, verify.password)
 
-    if(verify === null || verifyHash == false)
+    if(verify === null)
     {
 
       client.close();
@@ -105,9 +105,21 @@
     }
     else
     {
-      console.log("logged in");
-      client.close();
-      res.redirect('/gallery');
+      let verifyHash = false;
+      verifyHash = await bcrypt.compare(password, verify.password)
+
+      if(verifyHash == true)
+      {
+        console.log("logged in");
+        client.close();
+        res.redirect('/gallery');
+      }
+      else
+      {
+        client.close();
+        console.log("invalid data");
+        res.redirect('/invalidLogin');
+      }
     }
   });
   router.post('/signedUp', async (req, res) =>{
@@ -159,7 +171,7 @@
 
   // Generate the HTML for the images and the grid layout elements
   //let html = '<div id="images"><div class="grid">';
-  let html = "<style>body{    margin: 0;    padding: 0;    background-color: #111927;    overflow-x: hidden;    height: 100vh;    overflow-y: hidden;    overflow-x: hidden;}main{    width: 100vw;    height: 100%;        background-image: url(https://res.cloudinary.com/ddriyyppm/image/upload/v1672267405/background_rkrvc3.jpg);    background-repeat: no-repeat;    background-position: 0%;    }.btn-group-vertical{    backdrop-filter: blur(25px) saturate(100%) !important;    -webkit-backdrop-filter: blur(25px) saturate(100%) !important;    background-color: #121212d1 !important;    border-right: 2px solid rgba(23, 107, 181, 0.3) !important;    position: relative;    width: 4.5rem;    height: 100%;    float: left;}.btn-group-vertical button{    border-radius: 0;}.btn-group-vertical li{    background: rgba(3, 172, 240, 0) !important;    width: 4.5rem;    transition: 0.3s;}.btn-group-vertical li:hover{    backdrop-filter: blur(25px) saturate(200%) !important;    -webkit-backdrop-filter: blur(25px) saturate(200%) !important;    background-color: rgba(36, 59, 226, 0.8) !important;    width: 4.5rem;    transition: 0.3s;}.btn-group-vertical img{    filter: invert(98%) sepia(66%) saturate(6900%) hue-rotate(182deg) brightness(124%) contrast(92%);}#images{    backdrop-filter: blur(30px) saturate(100%) !important;    -webkit-backdrop-filter: blur(30px) saturate(100%) !important;    background-color: #121212d1 !important;    height: 100%;    width: calc(100vw - 4.5rem);    float: right;    overflow-y: scroll;    overflow-x: hidden;}#images img{    width: 80%;    height: 80%;    transition: .6s;    object-fit: cover;}#images img:hover{    width: 100%;    height: 100%;    transition: .6s;    border-radius: 12px;}.glass-border{    width: 100%;    height: 100%;    backdrop-filter: blur(4px) saturate(200%);    -webkit-backdrop-filter: blur(4px) saturate(200%);    background-color: rgba(0, 0, 0, 0.167);    border-radius: 12px;    border: 2px solid rgba(255, 255, 255, 0.125);        display: flex;    justify-content: center;}.glass{    background-size:cover !important;    background-repeat: no-repeat !important;    position: relative;    border-radius: 12px;    float: left;    display: flex;    justify-content: center;    transition: .3s;}    .grid:after {    content: '';    display: block;    clear: both;  }      .grid-item  {    float: left;    width: 25%;    height: 50vh;    min-width: 20rem;    max-width: 25%;  }    .grid-item--width2 {    width: 75%;     height: 50vh;     min-width: calc(100vh - 20rem);    max-width: 75%;    }  .grid-item--height2 {    width: 30rem;     height: 100vh;     min-width: 20rem;    max-width: 25%;    }    .grid-item:hover {    cursor: move;  }    .grid-item.is-dragging,  .grid-item.is-positioning-post-drag {    z-index: 2;    transition: .15s;  }    .packery-drop-placeholder {    background-color: rgba(250, 235, 215, 0.16);    -webkit-transition: -webkit-transform 0.5s;            transition: transform 0.5s;  }  #last  {    display: none;  }</style>";
+  let html = "<style>body{    margin: 0;    padding: 0;    background-color: #111927;    overflow-x: hidden;    height: 100vh;    overflow-y: hidden;    overflow-x: hidden;}main{    width: 100vw;    height: 100%;        background-image: url(./img/background.jpg);    background-repeat: no-repeat;    background-position: 0%;    }.btn-group-vertical{    backdrop-filter: blur(25px) saturate(100%) !important;    -webkit-backdrop-filter: blur(25px) saturate(100%) !important;    background-color: #121212d1 !important;    border-right: 2px solid rgba(23, 107, 181, 0.3) !important;    position: relative;    width: 4.5rem;    height: 100%;    float: left;}.btn-group-vertical button{    border-radius: 0;}.btn-group-vertical li{    background: rgba(3, 172, 240, 0) !important;    width: 4.5rem;    transition: 0.3s;}.btn-group-vertical li:hover{    backdrop-filter: blur(25px) saturate(200%) !important;    -webkit-backdrop-filter: blur(25px) saturate(200%) !important;    background-color: rgba(36, 59, 226, 0.8) !important;    width: 4.5rem;    transition: 0.3s;}.btn-group-vertical img{    filter: invert(98%) sepia(66%) saturate(6900%) hue-rotate(182deg) brightness(124%) contrast(92%);}#images{    backdrop-filter: blur(30px) saturate(100%) !important;    -webkit-backdrop-filter: blur(30px) saturate(100%) !important;    background-color: #121212d1 !important;    height: 100%;    width: calc(100vw - 4.5rem);    float: right;    overflow-y: scroll;    overflow-x: hidden;}#images img{    width: 80%;    height: 80%;    transition: .6s;    object-fit: cover;}#images img:hover{    width: 100%;    height: 100%;    transition: .6s;    border-radius: 12px;}.glass-border{    width: 100%;    height: 100%;    backdrop-filter: blur(4px) saturate(200%);    -webkit-backdrop-filter: blur(4px) saturate(200%);    background-color: rgba(0, 0, 0, 0.167);    border-radius: 12px;    border: 2px solid rgba(255, 255, 255, 0.125);        display: flex;    justify-content: center;}.glass{    background-size:cover !important;    background-repeat: no-repeat !important;    position: relative;    border-radius: 12px;    float: left;    display: flex;    justify-content: center;    transition: .3s;}    .grid:after {    content: '';    display: block;    clear: both;  }      .grid-item  {    float: left;    width: 25%;    height: 50vh;    min-width: 20rem;    max-width: 25%;  }    .grid-item--width2 {    width: 75%;     height: 50vh;     min-width: calc(100vh - 20rem);    max-width: 75%;    }    .grid-item--width3 {      width: 100%;       height: 50vh;       min-width: calc(100vh - 20rem);      max-width: 100%;      }  .grid-item--height2 {    width: 30rem;     height: 100vh;     min-width: 20rem;    max-width: 25%;    }    .grid-item:hover {    cursor: move;  }    .grid-item.is-dragging,  .grid-item.is-positioning-post-drag {    z-index: 2;    transition: .15s;  }    .packery-drop-placeholder {    background-color: rgba(250, 235, 215, 0.16);    -webkit-transition: -webkit-transform 0.5s;            transition: transform 0.5s;  }  #last  {    display: none;  }  .drag-area{    border: 3px dashed #fff;    height: 500px !important;    border-radius: 5px;    display: flex;    align-items: center;    justify-content: center;    flex-direction: column;    transition: 0.5s;  }  .drag-area.active{    transition: 0.5s;    border: 3px solid #fff;  }  .drag-area .icon{    font-size: 100px;    color: #fff;  }  .drag-area header{    font-size: 30px;    font-weight: 500;    color: #fff;  }  .drag-area span{    font-size: 25px;    font-weight: 500;    color: #fff;    margin: 10px 0 15px 0;  }  .drag-area button{    padding: 10px 25px;    font-size: 20px;    font-weight: 500;    border: none;    outline: none;    background: #fff;    color: rgba(41, 41, 41);    border-radius: 5px;    cursor: pointer;  }  .drag-area img{    height: 100%;    width: 100%;    object-fit: cover;    border-radius: 5px;  }</style>";
   const baseUrl = 'http://res.cloudinary.com/ddriyyppm/image/upload';
   for (const image of images) {
 
